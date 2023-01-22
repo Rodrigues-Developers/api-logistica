f8197aa6d029d5207422fd3c4c1f51055080b08f
Machado21 <grmachado12@hotmail.com>
2023-01-22 13:06:07 -0400

    feat:correct connecting

--- controllers/productConnection.js
+++ controllers/productConnection.js
@@ -16,17 +16,21 @@ const options = {
 //   useCreateIndex: true,
 // };
 
+
+
 const username = encodeURIComponent("machad721");
 const password = encodeURIComponent("CdIFkjddhM1ar7PK");
 const cluster = "logistics-cluster";
-const authSource = "admin";
+const database = "logistics";
 const authMechanism = "<authMechanism>";
 
-const DB_CONECTION = `mongodb+srv://${username}:${password}@${cluster}.y6c8dms.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${username}:${password}@${cluster}.y6c8dms.mongodb.net/${database}?retryWrites=true&w=majority`;
 
-const conn = mongoose.createConnection(DB_CONECTION, options); 
+const conn = mongoose.createConnection(uri, options); 
 
 //Se der errado olhar esta linha
 const ProductModel = conn.model("product", productSchema);
 
 module.exports = conn;
+
+
