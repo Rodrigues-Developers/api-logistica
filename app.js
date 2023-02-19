d26dd60aeae55e97bde84d43babe8fd02cb2712a
Bruno RC <brunorcx@hotmail.com>
2023-02-19 12:43:15 -0400

    fix: enable cors for all routes; add nodemon; added second MongooDB connection string

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -10,6 +11,9 @@ var productRouter = require("./routes/products");
 var logisticsRouter = require("./routes/logistics");
 
 var app = express();
+
+//Enable cors for all routes
+app.use(cors());
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -20,7 +24,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
