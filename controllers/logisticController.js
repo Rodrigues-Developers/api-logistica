418679c04549116d39ae569bbab2350c9d09cb54
Gabriel Rodrigues Machado <grmachado12@hotmail.com>
2024-04-17 10:02:26 -0400

    Update logisticController.js
    Allow CORs

--- controllers/logisticController.js
+++ controllers/logisticController.js
@@ -1,15 +1,17 @@
 const connection = require("./connection");
 const Logistics = connection.models["logistics"];
+const urlLocal = "http://localhost:4200"
+const urlOnline = "https://transporte-logistica.vercel.app"
 
 module.exports = {  
   async index(req, res) {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.setHeader("Access-Control-Allow-Origin", urlOnline);
     const data = await Logistics.find();
     return res.json(data);
   },
 
   async show(req, res) {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.setHeader("Access-Control-Allow-Origin", urlOnline);
     const data = await Logistics.findById(req.params.id);
     return res.json(data);
   },
